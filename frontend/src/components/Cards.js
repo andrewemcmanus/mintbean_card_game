d4ac66280775a43792ab125fa9d11fe0184bfb08
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-30 08:04:39 -0400

    set up state

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -5,46 +5,49 @@ import Deck from "../scripts/decks";
 export default function Cards() {
     const deck = new Deck();
     deck.shuffle();
-    let deckOne = [];
-    let deckTwo = [];
+    let initDeckOne = [];
+    let initDeckTwo = [];
     deck.cards.forEach((element, index) => {
         if (index % 2) {
-            deckOne.push(element)
+            initDeckOne.push(element)
         } else {
-            deckTwo.push(element);
+            initDeckTwo.push(element);
         }
     });
 
     // let length = deck.cards.length;
     
     const [ count, setCount ] = useState(0);
-    const [ tempDeckOne, setTempDeckOne ] = useState();
-    const [ tempDeckTwo, setTempDeckTwo ] = useState();
+    const [ deckOne, setDeckOne ] = useState(initDeckOne);
+    const [ deckTwo, setDeckTwo ] = useState(initDeckTwo);
     const handleCount = () => {
         setCount(prevCount => prevCount + 1);
-        returnWinner(count);
     }
     function returnWinner(input) {
         if (deckOne[input].number > deckTwo[input].number) {
             let lostCard = deckTwo.shift();
             deckOne.push(lostCard);
             deckOne.push(deckTwo[input]);
-            setTempDeckOne(deckOne);
-            console.log(tempDeckOne);
-            return tempDeckOne;
+            setDeckOne(deckOne);
+            console.log(deckOne);
+            return deckOne;
         };
         if (deckTwo[input].number > deckOne[input].number) {
             let lostCard = deckOne.shift();
             deckTwo.push(lostCard);
             deckTwo.push(deckOne[input]);
-            setTempDeckTwo(deckTwo);
-            console.log(tempDeckTwo);
-            return tempDeckTwo;
+            setDeckTwo(deckTwo);
+            console.log(deckTwo);
+            return deckTwo;
         };
     }
     return (
         <div>
-            <button onClick={handleCount}>next card {count}</button>
+            <button onClick={(e) => {
+                // e.preventDefault;
+                handleCount();
+                returnWinner(count);
+            }}>next card {count}</button>
             <Card number={deckOne[count].number} suit={deckOne[count].suit} />
             <Card number={deckTwo[count].number} suit={deckTwo[count].suit} />
         </div>
