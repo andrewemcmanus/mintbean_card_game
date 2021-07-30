18f3b6240a4347b21ab3b0628b8668fee716c1e6
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-30 10:04:17 -0400

    War: script or separate component?

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -9,47 +9,60 @@ export default function Cards() {
     let initDeckTwo = [];
     deck.cards.forEach((element, index) => {
         if (index % 2) {
-            initDeckOne.push(element)
+            initDeckOne.push(element);
         } else {
             initDeckTwo.push(element);
         }
     });
-
-    // let length = deck.cards.length;
     
-    const [ count, setCount ] = useState(0);
+    // const [ count, setCount ] = useState(0);
     const [ deckOne, setDeckOne ] = useState(initDeckOne);
     const [ deckTwo, setDeckTwo ] = useState(initDeckTwo);
-    const handleCount = () => {
-        setCount(prevCount => prevCount + 1);
-    }
+    // const handleCount = () => {
+    //     setCount(prevCount => prevCount + 1);
+    // }
     function returnWinner(input) {
         if (deckOne[input].number > deckTwo[input].number) {
             let lostCard = deckTwo.shift();
+            deckOne.push(deckOne.shift());
             deckOne.push(lostCard);
-            deckOne.push(deckTwo[input]);
             setDeckOne(deckOne);
-            console.log(deckOne);
+            setDeckTwo(deckTwo);
+            // setCount(0);
+            if (deckOne.length > 51) {
+                alert("Player 1 wins!");
+                return deckOne;
+            }
+            console.log(`Player 1 won: ${deckOne.length} Player 2 lost: ${deckTwo.length}`);
             return deckOne;
         };
         if (deckTwo[input].number > deckOne[input].number) {
             let lostCard = deckOne.shift();
+            deckTwo.push(deckTwo.shift());
             deckTwo.push(lostCard);
-            deckTwo.push(deckOne[input]);
             setDeckTwo(deckTwo);
-            console.log(deckTwo);
+            setDeckOne(deckOne);
+            // setCount(0);
+            if (deckTwo.length > 51) {
+                alert("Player 2 wins!");
+                return deckTwo;
+            };
+            console.log(`Player 2 won: ${deckTwo.length} Player 1 lost: ${deckOne.length}`);
             return deckTwo;
         };
+        
     }
+    // For War: script or component?
     return (
         <div>
-            <button onClick={(e) => {
+            <button onClick={() => {
                 // e.preventDefault;
-                handleCount();
-                returnWinner(count);
-            }}>next card {count}</button>
-            <Card number={deckOne[count].number} suit={deckOne[count].suit} />
-            <Card number={deckTwo[count].number} suit={deckTwo[count].suit} />
+                // handleCount();
+                returnWinner(0);
+            }}>next card</button>
+            {/* issue with count and array length? */}
+            <Card number={deckOne[0].number} suit={deckOne[0].suit}/>
+            <Card number={deckTwo[0].number} suit={deckTwo[0].suit} />
         </div>
     )
 }
